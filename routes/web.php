6fc4075554257416227b3d881e20cd79ffabfecd
tsune-s <75601784+tsune-s@users.noreply.github.com>
2025-04-19 16:15:28 +0900

    test: add fallback route to check if Laravel is working

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Laravel is working!';
 });
-
-use App\Http\Controllers\MemoController;
-
-Route::get('/', [MemoController::class, 'index']);
-Route::post('/store', [MemoController::class, 'store']);
-
-
-use App\Http\Controllers\TodoController;
-
-Route::get('/', [TodoController::class, 'index']);
-Route::post('/store', [TodoController::class, 'store']);
-Route::post('/toggle/{id}', [TodoController::class, 'toggle']);
-Route::post('/delete/{id}', [TodoController::class, 'destroy']);
-
-
-use App\Http\Controllers\OmikujiController;
-
-Route::get('/omikuji', [OmikujiController::class, 'index']);
-Route::post('/omikuji/result', [OmikujiController::class, 'result']);
